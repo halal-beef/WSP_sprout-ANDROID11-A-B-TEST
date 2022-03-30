d9ab4b3e06ecad57309e5391d0df55a2d50a224a
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2022-03-30 19:07:45 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_WSP_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_WSP_sprout-user \
-    twrp_WSP_sprout-userdebug \
-    twrp_WSP_sprout-eng
+    twrp_WSP_poo-user \
+    twrp_WSP_poo-userdebug \
+    twrp_WSP_poo-eng
